063b86fff6df5c0413de3ec605fb7986aa410aa7
Erlend <elandroe@icloud.com>
2017-08-09 14:34:11 +0200

    style posts

--- readable/src/components/App.js
+++ readable/src/components/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    categories: []
+    categories: [],
+    posts: []
   }
 
   componentWillMount() {
-    fetch('http://localhost:5001/categories', { headers: { 'Authorization': 'whatever-you-want' }})
+    const serverPath = 'http://localhost:5001';
+    const authorizationHeader = { 'Authorization': 'myKey' };
+
+    fetch(`${serverPath}/categories`, { headers: authorizationHeader })
       .then(res => res.json())
-      .then(res => this.setState({ categories: res.categories }))
+      .then(res => this.setState({ categories: res.categories }));
+
+    fetch(`${serverPath}/posts`, { headers: authorizationHeader })
+      .then(res => res.json())
+      .then(res => this.setState({ posts: res }));
+  }
+
+  getFormattedDate(unixTime) {
+    const dateObj = new Date(unixTime);
+    return `${dateObj.getDate()}-${dateObj.getMonth()}-${dateObj.getFullYear()}`
   }
 
   render() {
@@ -23,6 +36,26 @@ class App extends Component {
               )
             }
           </nav>
+        </div>
+        <div className="posts">
+          {
+            this.state.posts.map(post => (
+              <div className="posts__post" key={post.id}>
+                <div className="posts__post-info">
+                  <div>
+                    <p className="posts__post-title">{post.title}</p>
+                    <p className="posts__post-time">{this.getFormattedDate(post.timestamp)}</p>
+                  </div>
+                  <span className="posts__post-author">Author: {post.author}</span>
+                </div>
+                <div className="posts__votes">
+                  <span>&#129093;</span>
+                  <span className="posts__vote-score">{post.voteScore}</span>
+                  <span>&#129095;</span>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     );
