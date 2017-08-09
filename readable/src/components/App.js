cb49e15f9b811fe4db02cc1ed177546af4cb4814
Erlend <elandroe@icloud.com>
2017-08-09 11:34:47 +0200

    fetch categories from server

--- readable/src/components/App.js
+++ readable/src/components/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 class App extends Component {
+  state = {
+    categories: []
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:5001/categories', { headers: { 'Authorization': 'whatever-you-want' }})
+      .then(res => res.json())
+      .then(res => this.setState({ categories: res.categories }))
+  }
+
   render() {
     return (
-      <h1>Hello</h1>
+      <ul>
+        {
+          this.state.categories.map(category => <li key={category.name}>{category.name}</li>)
+        }
+      </ul>
     );
   }
 }
