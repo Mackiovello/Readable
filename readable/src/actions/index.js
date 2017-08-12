e5f3ede1c987094cad173f59cc48a44920f7b45f
Erlend <elandroe@icloud.com>
2017-08-12 14:02:41 +0200

    implement voting with server

--- readable/src/actions/index.js
+++ readable/src/actions/index.js
@@ -4,18 +4,49 @@ export const CREATE_POST = "CREATE_POST";
 export const CREATE_CATEGORY = "CREATE_CATEGORY";
 
 export function upvote(post) {
+  return dispatch => vote(post, "upVote", dispatch);
+}
+
+export function downvote(post) {
+  return dispatch => vote(post, "downVote", dispatch);
+}
+
+function downvoteAction(post) {
+  return {
+    type: DOWNVOTE,
+    post
+  };
+}
+
+function upvoteAction(post) {
   return {
     type: UPVOTE,
     post
   };
 }
 
-export function downvote(post) {
-  return {
-    type: DOWNVOTE,
-    post
-  };
+const vote = (post, option, dispatch) => {
+  return fetch(`http://localhost:5001/posts/${post.id}`,
+    { 
+      method: "post", 
+      body: JSON.stringify({ option }),
+      headers: { 
+        Authorization: "myKey", 
+        "Content-Type": "application/json" 
+      }, 
+    }).then(
+    () => {
+      if (option === "upVote") {
+        dispatch(upvoteAction(post));
+      } else if (option === "downVote") {
+        dispatch(downvoteAction(post));
+      } else {
+        throw new Error("option must be 'upVote' or 'downVote'");
+      }
+    }
+  )
 }
+
 
 export function createCategory(category) {
   return {
--- readable/src/index.js
+++ readable/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import posts from "./reducers/posts";
 import categories from "./reducers/categories";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { reducer as reduxFormReducer } from "redux-form";
+import thunk from "redux-thunk";
 
 const combinedReducers = combineReducers({
   form: reduxFormReducer,
@@ -18,8 +19,10 @@ const combinedReducers = combineReducers({
 
 const store = createStore(
   combinedReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
+
 
 ReactDOM.render(
   <BrowserRouter>
