2f6cb2677d52538575a830849773f9f3f7192da4
Erlend <elandroe@icloud.com>
2017-08-12 18:55:54 +0200

    style post page

--- readable/src/components/Post.js
+++ readable/src/components/Post.js
@@ -3,11 +3,19 @@ import "../styles/Post.css";
 
 export default class Post extends Component {
   render() {
-    const { title, body } = this.props.post;
+    const { title, body, author } = this.props.post;
     return (
       <div>
         <div className="post-card">
+          <h2 className="post-card__title">{title}</h2>
           <p className="post-card__body">{body}</p>
+          <div className="post-card__footer">
+            <span>{author}</span>
+            <div>
+              <button className="button">delete</button>
+              <button className="button">edit</button>
+            </div>
+          </div>
         </div>
       </div> 
     )
