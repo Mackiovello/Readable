697577696af2c00157ae2bc5aa553c897a51ce6c
Erlend <elandroe@icloud.com>
2017-09-24 18:17:55 -0500

    refactor Comments component into a function component

--- readable/src/components/Comments.js
+++ readable/src/components/Comments.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { getFormattedDate } from "../helpers";
 import Votes from "./Votes";
@@ -10,54 +10,45 @@ import {
 import Actions from "./Actions";
 import { Comment, CommentWrapper, CommentInfo } from "./StyledComponents";
 
-class Comments extends Component {
-  render() {
-    const { comments, parentId, deleteComment } = this.props;
-    const filteredComments = parentId
-      ? comments.filter(comment => comment.parentId === parentId)
-      : comments;
+function Comments({ comments, parentId, deleteComment }) {
+  const filteredComments = parentId
+    ? comments.filter(comment => comment.parentId === parentId)
+    : comments;
 
-    return (
-      <div>
-        {filteredComments.map(comment =>
-          <Comment key={comment.id}>
-            <CommentWrapper>
-              <p style={{margin: "0 0 15px 0"}}>
-                {comment.body}
-              </p>
-              <CommentInfo>
-                <span>
-                  {comment.author}
-                </span>
-                <span>
-                  {getFormattedDate(comment.timestamp)}
-                </span>
-              </CommentInfo>
-            </CommentWrapper>
-            <Actions
-              editLink={`/comment/${comment.id}/edit`}
-              onDelete={() => deleteComment(comment)}
-            />
-            <Votes
-              toVoteOn={comment}
-              upvote={upvoteComment}
-              downvote={downvoteComment}
-            />
-          </Comment>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {filteredComments.map(comment =>
+        <Comment key={comment.id}>
+          <CommentWrapper>
+            <p style={{margin: "0 0 15px 0"}}>
+              {comment.body}
+            </p>
+            <CommentInfo>
+              <span>
+                {comment.author}
+              </span>
+              <span>
+                {getFormattedDate(comment.timestamp)}
+              </span>
+            </CommentInfo>
+          </CommentWrapper>
+          <Actions
+            editLink={`/comment/${comment.id}/edit`}
+            onDelete={() => deleteComment(comment)}
+          />
+          <Votes
+            toVoteOn={comment}
+            upvote={upvoteComment}
+            downvote={downvoteComment}
+          />
+        </Comment>
+      )}
+    </div>
+  );
 }
 
-function mapStateToProps({ comments }) {
-  return { comments };
-}
-
-function mapDispatchToProps(dispatch) {
+export default connect(({ comments }) => ({ comments }), dispatch => {
   return {
     deleteComment: comment => dispatch(deleteCommentAction(comment))
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Comments);
+  }
+})(Comments);
