0e43ce3b8e9d9dcf4b6af4a75520ea6e13b0e36b
Erlend <elandroe@icloud.com>
2017-09-26 19:20:07 -0500

    setup basic starting point for dotnet Web API

---
+++ ReadableApi/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadableApi.Models
+{
+    public class Post
+    {
+        public DateTime Timestamp { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public int VoteScore { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
---
+++ ReadableApi/TempDataStore.cs
@@ -0,0 +1,42 @@
+﻿using ReadableApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadableApi
+{
+    public class TempDataStore
+    {
+        public static TempDataStore Current { get; } = new TempDataStore();
+
+        public List<Post> Posts { get; set; }
+
+        public TempDataStore()
+        {
+            Posts = new List<Post>()
+            {
+                new Post()
+                {
+                    Author = "Gandalf",
+                    Title = "There and Back Again",
+                    Timestamp = DateTime.Now,
+                    Body = "This is a long long story about a Hobbit...",
+                    Category = "Fantasy",
+                    VoteScore = 4000,
+                    Deleted = false
+                },
+                new Post()
+                {
+                    Author = "Sam",
+                    Title = "No More Volcanoes",
+                    Timestamp = DateTime.Now,
+                    Body = "I'm done dude, seriously. No more volcanoes.",
+                    Category = "Fantasy",
+                    VoteScore = 8420,
+                    Deleted = false
+                },
+            };
+        }
+    }
+}
